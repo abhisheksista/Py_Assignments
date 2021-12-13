03d8ce4d14c036428cfdbcfaa51c0e24bdb117e7
abhisheksista <89856036+abhisheksista@users.noreply.github.com>
2021-12-13 11:28:59 +0530

    Create Assignment_1.py

---
+++ Assignment_1.py
@@ -0,0 +1,20 @@
+# 10,1,8,3,6,5,4,7,x,y
+# The general solution for x is x=n-5 where n is previous number
+# The general solution for y is y=n+7 where n is previous number. So y=x+7
+# Algorithm
+# step1: start
+# step2: Find the value of n
+# step3: Use x=n-5 to find the value of x
+# step4: Use y=x+7 to find the value of y
+# step5: stop
+
+# Floor division
+x=9
+y=3
+print(x//y)
+a=9
+b=4
+print(a//b)
+c=9
+d=10
+print(c//d)

2cf67b02c219965ba57fb752fdfdeb9aa2fee7f1
abhisheksista <89856036+abhisheksista@users.noreply.github.com>
2021-12-19 11:51:35 +0530

    Create Assignment_2.py

---
+++ Assignment_2.py
@@ -0,0 +1,13 @@
+x=int(input("what is starting or ending year of friends sitcom"))
+if x==1994 or x==2004:
+    print("correct")
+else:
+    print("wrong")
+print("the negation of true is",not(True))
+
+x=int(input("enter a number: "))
+y=int(input("enter a number: "))
+if x>y:
+    print("number 1 is greater than number 2")
+else:
+    print("number 2 is greater than number 1")
